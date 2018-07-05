de8069936348252f9269f89838a7760d814bbf86
Arda Akdemir <ardaakdemir1@gmail.com>
2018-07-05 22:49:02 +0300

    Add files via upload
    Metadata deletion completed

--- aceconv2.py
+++ aceconv2.py
@@ -4,39 +4,53 @@ import xml.etree.ElementTree as ET
 import pickle
 import sys
 
+
 def save_pickle(data,outfile):
     pickle.dump( data, open( outfile, "wb" ) )
 def load_pickle(filename):
     return pickle.load( open( filename, "rb" ) )
 
 def xmltointer(xmlfile,rawfile,entypes):
-    tree = ET.parse(xmlfile)
-    treeraw=etree.parse(rawfile)
-    rootraw=treeraw.getroot()
-    root = tree.getroot()
-    names=[]
-    inds=[]
-    typel=[]
-    for doc in root.findall('document'):
-        for entity in doc.findall('entity'):
-            type1= entity.get('TYPE')
-            if type1 in entypes:
-                mentions=entity.findall('entity_mention')
-                for ment in mentions:
-                    mentype1=ment.get("TYPE")
-                    if mentype1=="NAM":
-                        extent1=ment.find("head")
-                        charseq1=extent1.find("charseq")
-                        names.append(charseq1.text)
-                        inds.append((int(charseq1.get('START'))-1,int(charseq1.get('END'))))
-                        typel.append(type1)
-    body=rootraw.find("BODY")
-    tex=[x for x in rootraw.itertext()]
-    tex[0]=tex[0][1:]
-    text1=""
-    for t in tex:
-        text1=text1+t
-    return text1, names, inds ,typel
+	tree = ET.parse(xmlfile)
+	treeraw=etree.parse(rawfile)
+	rootraw=treeraw.getroot()
+	root = tree.getroot()
+	names=[]
+	inds=[]
+	typel=[]
+	for doc in root.findall('document'):
+		for entity in doc.findall('entity'):
+		    type1= entity.get('TYPE')
+		    if type1 in entypes:
+		        mentions=entity.findall('entity_mention')
+		        for ment in mentions:
+		            mentype1=ment.get("TYPE")
+		            if mentype1=="NAM":
+		                extent1=ment.find("head")
+		                charseq1=extent1.find("charseq")
+		                names.append(charseq1.text)
+		                inds.append((int(charseq1.get('START'))-1,int(charseq1.get('END'))))
+		                typel.append(type1)
+	body=rootraw.find("BODY")
+	tex=[x for x in rootraw.itertext()]
+	docid=rootraw.find("DOCID").text
+	doctype=rootraw.find("DOCTYPE").text
+	doctime=rootraw.find("DATETIME").text
+	body=rootraw.find("BODY")
+	headl=body.find("HEADLINE")
+	head="heeead"
+	if headl!=None:
+		#print(headl.text)
+		head=headl.text
+	text1=""
+	for x in tex[1:]:
+		if x!=docid and x!=doctype and x!=doctime and x!=head:
+			text1+=x
+		else:
+			text1+="!1"+x[2:-2]+"!1"
+	#for t in tex:
+	#   text1=text1+t
+	return text1, names, inds ,typel
 
 ##in order this function to work
 ## the address of the file.tbl file must be givene
@@ -133,7 +147,7 @@ def subtractsub(str1,sub1):
 
 ##takes as input the marked rawtext and outputs the token-per-line corpus
 ## the code is not neat and will be updated
-def markedtotokenperline(markedtext,entypes,outname):
+def markedtotokenperline(markedtext,entypes,outname,tagver):
     tags=[]
     out=open(outname,"w")
     for t1 in entypes:
@@ -157,9 +171,15 @@ def markedtotokenperline(markedtext,entypes,outname):
                     singtok=subtractsub(w1,"1"+tag)
                     if singtok!="-1":
                         f=0
-                        out.write(singtok+" "+tag+"\n")
+                        if tagver==0:
+                        	out.write(singtok+" 1\n")
+                        else:
+                        	out.write(singtok+" "+tag+"\n")
                     else:
-                        out.write(w1+" "+tag+"\n")
+                    	if tagver==0:
+                    		out.write(w1+" 1\n")
+                    	else:
+                        	out.write(w1+" "+tag+"\n")
             else:
                 for t1 in tags:
                     if t1 in word:
@@ -168,24 +188,81 @@ def markedtotokenperline(markedtext,entypes,outname):
                         break
                 if f==0:
                     w1=subtractsub(word,"1"+tag)
-                    out.write(w1+" "+tag+"\n")
+                    if tagver==0:
+                    	out.write(w1+" 1\n")
+                    else:
+                    	out.write(w1+" "+tag+"\n")
                 else:
-                    out.write(word+" "+tag+"\n")
-        out.write("\n")   
+                	if tagver==0:
+                		out.write(word+" 1\n")
+                	else:       		
+                		out.write(word+" "+tag+"\n")
+        out.write("\n")  
+##delete all characters between special signs 
+def deletemetadata(corpus,specsign):
+    ind1=corpus.find(specsign)
+    ind2=0
+    ind=0
+    text=""
+    indprev=0
+    while ind1<len(corpus)-5:
+        ind2=corpus[ind1+1:].find(specsign)+ind1+1
+        text+=corpus[ind:ind1]
+        ind1=corpus[ind2+1:].find(specsign)+ind2+1
+        ind=ind2+2
+        if ind1-ind2-1==-1:
+            break
+    return text
 
+
+
+##save these variables
+def ACEtotokenperlineconverter(filelistfile,interoutfilename,outfilename,tagtypes,tagver):
+	filelist=filenamelist(filelistfile)
+	t,names,inds,typelist=ACEtoInterCorpus(filelistfile,outfilename,tagtypes)
+	markedtext=markentities(t,names,inds,typelist,interoutfilename)
+	deletedt=deletemetadata(markedtext,"!1")
+	markedtotokenperline(deletedt,tagtypes,outfilename,tagver)
+	##metadataeraser not working properly because of tokenization
+	###metadataeraser.deletemetadata(outfilename,filelist,"deletedcorp")
+    
 base="ACE/aceCorp/"
 filelistfile="ACE/aceCorp/docs/file.tbl"
-outn="tokperline"
+intoutn="out1"
+outn="tokperlineCorpus"
 tagtypes=["ORG","LOC","GPE"]
-tagver=0           
-##save these variables
-print("filelist address: ACE/aceCorp/docs/file.tbl")
-print("intermediate raw corpus: out1")
-print("output file name: tokperline")
-print("Tag types: ORG LOC GPE")
-t,names,inds,typelist=ACEtoInterCorpus(filelistfile,outfilename,tagtypes)
-markedtext=markentities(t,names,inds,typelist,"out1")
-markedtotokenperline(markedtext,tagtypes,outn)
+tagver=1         
+metadata=1
 
+
+##save these variables
+
+
+
+
+args=sys.argv
+if len(args)>1:
+	if args[1]=="-h":
+		print("python3 aceconv2.py filelistfile outfilename tagtypes tagver | for custom mode")
+		print("python3 aceconv2.py def  | for default mode")
+	elif args[1]=="def":
+		print("filelist address: ACE/aceCorp/docs/file.tbl")
+		print("intermediate raw corpus: out1")
+		print("output file name: tokperlineCorpus")
+		print("Tag types: ORG LOC GPE")
+		print("Tagging version: type-specific")
+		print("metadata deleted")
+		ACEtotokenperlineconverter(filelistfile,intoutn,outn,tagtypes,tagver)
+			
+	else:
+		tagtypes1=[]
+		filelistfile1=args[1]
+		outfilename1=args[2]
+		interoutfilename="out1"
+		for x in args[3:-1]:
+			tagtypes1.append(x)
+		tagver1=int(args[-1])
+		#metadata1=int(args[-1])
+		ACEtotokenperlineconverter(filelistfile1,interoutfilename,outfilename1,tagtypes1,tagver1)
 
 
